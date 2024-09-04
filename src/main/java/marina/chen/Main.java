247250a8f7a6b5d7c83d705c3dd8e5c0c4d904da
Marina Chen <chen.marina.vm10@gmail.com>
2024-09-04 23:28:51 +0500

    Update Main.java

--- src/main/java/marina/chen/Main.java
+++ src/main/java/marina/chen/Main.java
@@ -3,5 +3,6 @@ package marina.chen;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Salom Tashkent!");
+        System.out.println("Weclome to Tashkent!");
     }
 }
