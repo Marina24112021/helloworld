c82945b3e105a80cec8f00a9106c675ca54c5b50
Marina Chen <chen.marina.vm10@gmail.com>
2024-09-04 23:31:40 +0500

    create new method printVal

--- src/main/java/marina/chen/Main.java
+++ src/main/java/marina/chen/Main.java
@@ -2,6 +2,9 @@ package marina.chen;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Salom Tashkent!");
+        printVal("Hi Tashkent");
+    }
+    public static String printVal( String str ) {
+        return ( str );
     }
 }
