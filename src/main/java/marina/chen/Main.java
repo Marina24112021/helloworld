841bec0cb79c235b12799bc73a9fad269f338f8a
Marina Chen <chen.marina.vm10@gmail.com>
2024-09-07 23:27:13 +0500

    Add some limes like Salom Tashkent

--- src/main/java/marina/chen/Main.java
+++ src/main/java/marina/chen/Main.java
@@ -1,7 +1,12 @@
 package marina.chen;
 
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Salom Tashkent!");
+        System.out.println("Salom Tashkent!");
+        System.out.println("Salom Tashkent!");
         System.out.println("Salom Tashkent!");
     }
 }
